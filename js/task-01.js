bc5f2857f95e1005118004ae7781c9fcde9dfefa
ZjeniiaSt <zeniiastupjak@gmail.com>
2021-09-15 21:31:14 +0300

    1+

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,9 @@
 const categoriesItemEl = document.querySelectorAll(".item");
 console.log(`В списке ${categoriesItemEl.length} категории`);
 
-console.log(
-  `Категория: ${categoriesItemEl[0].querySelector("h2").textContent} `
-);
-console.log(
-  `Количество элементов: ${categoriesItemEl[0].querySelectorAll("li").length} `
-);
-
-console.log(
-  `Категория: ${categoriesItemEl[1].querySelector("h2").textContent} `
-);
-console.log(
-  `Количество элементов: ${categoriesItemEl[1].querySelectorAll("li").length} `
-);
-
-console.log(
-  `Категория: ${categoriesItemEl[2].querySelector("h2").textContent} `
-);
-console.log(
-  `Количество элементов: ${categoriesItemEl[2].querySelectorAll("li").length} `
+categoriesItemEl.forEach((i) =>
+  console.log(
+    `Категория: ${i.querySelector("h2").textContent} 
+    Количество элементов: ${i.querySelectorAll("li").length} `
+  )
 );
