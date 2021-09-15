0b32d320818bc910224044770c3210a826ce81ba
ZjeniiaSt <zeniiastupjak@gmail.com>
2021-09-15 20:41:00 +0300

    1 - 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+ingredientsListEl.append(
+  ...ingredients.map((ingredient) => {
+    const ingredientItemEl = document.createElement("li");
+    ingredientItemEl.textContent = ingredient;
+    return ingredientItemEl;
+  })
+);
